525e5f2b9fb8d0c620a7702c9d0846c49526c57e
skr <125522005+popsy-y@users.noreply.github.com>
2024-04-16 23:38:05 +0900

    戦闘時の行動決定ロジックに必要な判断材料を取得できるよう実装

--- Assets/D-Sakurai/Resources/Skills/PersonalitySkills.cs
+++ Assets/D-Sakurai/Resources/Skills/PersonalitySkills.cs
@@ -5,7 +5,8 @@ namespace D_Sakurai.Resources.Skills
     [CreateAssetMenu(fileName = "PersonalitySkills", menuName = "CombatSystem/PersonalitySkills", order = 0)]
     public class PersonalitySkills : ScriptableObject
     {
-        public SkillBase.BraverSkillData[] Active;
-        public SkillBase.BraverSkillData[] Sociable;
+        public SkillBase.BraverSkillData[] PersonalitySkillArray;
+        // public SkillBase.BraverSkillData[] Active;
+        // public SkillBase.BraverSkillData[] Sociable;
     }
 }
