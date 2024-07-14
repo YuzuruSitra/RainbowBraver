3ac4c1afd8ee59a0b69022b0dcce7e63ecafe296
Sitra <yuzurusitra@gmail.com>
2024-07-14 11:32:06 +0900

    友好度の変数を用意

--- Assets/D-yuzuki/Scripts/RoomCharacters/NPC/Braver/BraverParameter.cs
+++ Assets/D-yuzuki/Scripts/RoomCharacters/NPC/Braver/BraverParameter.cs
@@ -1,5 +1,5 @@
 using System;
-using Unity.Mathematics;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace D_yuzuki.Scripts.RoomCharacters.NPC.Braver
@@ -30,16 +30,38 @@ namespace D_yuzuki.Scripts.RoomCharacters.NPC.Braver
         // 今後ロード予定
         private int _braverCount = 1;
         public float[,] Parameters { get; private set; }
-        
+        public List<List<float>> Friendship { get; private set; }
         private void Start()
         {
             Parameters = new float[_braverCount, Enum.GetValues(typeof(Parameter)).Length];
+            InitializeFriendship(_braverCount);
+        }
+
+        private void InitializeFriendship(int braverCount)
+        {
+            Friendship = new List<List<float>>(braverCount);
+            for (var i = 0; i < braverCount; i++)
+            {
+                Friendship.Add(new List<float>(braverCount));
+                for (var j = 0; j < braverCount; j++)
+                {
+                    Friendship[i].Add(0f); // 初期値として0を設定
+                }
+            }
         }
 
         public void UpdateStatus(int braverNum, Parameter targetParam, float newValue)
         {
             Parameters[braverNum, (int)targetParam] = newValue;
         }
+        
+        public void UpdateFriendship(int braverNum, int targetNum, float newValue)
+        {
+            if (braverNum >= Friendship.Count || targetNum >= Friendship[braverNum].Count) return;
+            Friendship[braverNum][targetNum] = newValue;
+        }
+        
+        // ブレーバーの人数が変わったとき用の処理を追記予定
         
     }
 }
